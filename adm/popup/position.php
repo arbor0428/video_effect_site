456d2353cf347ed45778ae7429645cbf8793982e
바가나 <86461360+whynot1217@users.noreply.github.com>
2023-06-27 13:27:28 +0900

    Delete adm/popup directory

--- adm/popup/position.php
+++
@@ -1,5 +0,0 @@
-<table  height='100%' width='100%' border="0" cellspacing="0" cellpadding="0" align='center' valign='middle' onclick='self.close();' style='cursor:hand;'>
-	<tr>
-		<td align='center' valign='middle'><img src='./img/pop_position1.jpg'></td>
-	</tr>
-</table>
